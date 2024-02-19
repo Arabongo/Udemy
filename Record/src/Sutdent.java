641670c434db2bf549ebc9cf31b81cb072fc1ea8
Arabongo <edosurfer118@gmail.com>
2024-02-19 14:23:44 +0100

    fifth commit

---
+++ Record/src/Sutdent.java
@@ -0,0 +1,7 @@
+public record Sutdent(String id, String name, String dateOfBirth, String classList) {
+
+    //i record non supportano i getter e setter ma hanno incorporato il costruttore con i parametri inseriti tra
+    //le parentesi tonde implicitamente
+    // i parametri sono final e private
+    //immutabile
+}
